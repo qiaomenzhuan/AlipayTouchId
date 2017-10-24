fe1258b3cb2a840da738a2376b414e46de8aaa7d
qiaomenzhuan <1308332927@qq.com>
2017-10-24 11:03:50 +0800

    支付宝 指纹解锁 Alipay Touch Id
    高仿支付宝指纹解锁Alipay Touch Id

---
+++ 指纹识别/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  指纹识别
+//
+//  Created by 杨磊 帅™ on 2017/10/24.
+//  Copyright © 2017年 YangLei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
--- 指纹识别/ViewController.m
+++ 指纹识别/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
-
+#import <LocalAuthentication/LocalAuthentication.h>
+#import <AudioToolbox/AudioToolbox.h>
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *touchBtn;
 
 @end
 
@@ -16,7 +18,39 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)touch:(id)sender {
+    if ([UIDevice currentDevice].systemVersion.floatValue < 8.0)
+    {
+        return;
+    }
+    LAContext *ctx = [[LAContext alloc] init];
+    // 判断设备是否支持指纹识别
+    if ([ctx canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:NULL]) {
+        // 输入指纹 - 回调是异步的
+        [ctx evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:@"通过Home键验证已有手机指纹" reply:^(BOOL success, NSError *error)
+         {
+             if (success)
+             {
+                 //成功
+                 UIAlertController *alertController=[UIAlertController alertControllerWithTitle:@"恭喜你" message:@"解锁成功!" preferredStyle:UIAlertControllerStyleAlert];
+                 UIAlertAction *confirmAction=[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil];
+                 [alertController addAction:confirmAction];
+                 [self presentViewController:alertController animated:YES completion:nil];
+                 
+             } else
+             {
+                 AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+                 // 判断错误类型是否是主动自行输入密码
+                 if (error.code == LAErrorUserFallback)
+                 {
+                     dispatch_async(dispatch_get_main_queue(), ^{
+                         //密码验证方法
+                     });
+                 }
+             }
+         }];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
